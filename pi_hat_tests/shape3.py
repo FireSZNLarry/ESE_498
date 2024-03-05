96d785b5250caad240c0980d9d3fff7257d27c1d
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-03-05 14:06:56 -0600

    Update shape3.py

--- pi_hat_tests/shape3.py
+++ pi_hat_tests/shape3.py
@@ -1,9 +1,29 @@
 import cv2
 from matplotlib import pyplot as plt
+
+# open camera
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+
+# set dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+
+# take frame
+ret, frame = cap.read()
+#flip frame takes pictures right side up
+flipped_frame = cv2.flip(frame,0)
+
+# write frame to file
+cv2.imwrite('flipped_image.jpg', flipped_frame)
+# release camera
+cap.release()
+
+
+
    
    
 # Opening image
-img = cv2.imread("image.jpg")
+img = cv2.imread("flipped_image.jpg")
    
 # OpenCV opens images as BRG 
 # but we want it as RGB and 
