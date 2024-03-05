8b597e7a9dcba1d96d15e0bdfc5e33e6040811d3
ckrobinsonwustl <ckrobinson@wustl.edu>
2024-03-05 13:40:32 -0600

    Update shape1.py

--- pi_hat_tests/shape1.py
+++ pi_hat_tests/shape1.py
@@ -51,25 +51,25 @@ for contour in contours:
         contour, 0.01 * cv2.arcLength(contour, True), True) 
       
     # using drawContours() function 
-cv2.drawContours(img, [contour], 0, (0, 0, 255), 5) 
+    cv2.drawContours(img, [contour], 0, (0, 0, 255), 5) 
   
-# finding center point of shape 
-M = cv2.moments(contour) 
-if M['m00'] != 0.0: 
-  x = int(M['m10']/M['m00']) 
-  y = int(M['m01']/M['m00']) 
+    # finding center point of shape 
+    M = cv2.moments(contour) 
+    if M['m00'] != 0.0: 
+      x = int(M['m10']/M['m00']) 
+      y = int(M['m01']/M['m00']) 
 
-# putting shape name at center of each shape 
-if len(approx) == 3: 
-  cv2.putText(img, 'Triangle', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
-elif len(approx) == 4: 
-  cv2.putText(img, 'Quadrilateral', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
-elif len(approx) == 5: 
-  cv2.putText(img, 'Pentagon', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
-elif len(approx) == 6: 
-  cv2.putText(img, 'Hexagon', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
-else: 
-  cv2.putText(img, 'circle', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+    # putting shape name at center of each shape 
+    if len(approx) == 3: 
+      cv2.putText(img, 'Triangle', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+    elif len(approx) == 4: 
+      cv2.putText(img, 'Quadrilateral', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+    elif len(approx) == 5: 
+      cv2.putText(img, 'Pentagon', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+    elif len(approx) == 6: 
+      cv2.putText(img, 'Hexagon', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
+    else: 
+      cv2.putText(img, 'circle', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
   
 # displaying the image after drawing contours 
 cv2.imshow('shapes', img) 
