2dbcd419351e67103b45cc48aa65197f619b3ff6
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-03-05 14:21:12 -0600

    Update shape3.py

--- pi_hat_tests/shape3.py
+++ pi_hat_tests/shape3.py
@@ -20,7 +20,7 @@ cap.release()
 
 
 # import cascade file for facial recognition
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
 
 '''
     # if you want to detect any object for example eyes, use one more layer of classifier as below:
