3a875359be0cc4ad4a5791e22b267a73ec44b9a7
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-03-05 14:15:47 -0600

    Update shape3.py

--- pi_hat_tests/shape3.py
+++ pi_hat_tests/shape3.py
@@ -1,78 +1,38 @@
 import cv2
-from matplotlib import pyplot as plt
 
-# open camera
-cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+# Enable camera
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 420)
 
-# set dimensions
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+# import cascade file for facial recognition
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-# take frame
-ret, frame = cap.read()
-#flip frame takes pictures right side up
-flipped_frame = cv2.flip(frame,0)
+'''
+    # if you want to detect any object for example eyes, use one more layer of classifier as below:
+    eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
+'''
 
-# write frame to file
-cv2.imwrite('flipped_image.jpg', flipped_frame)
-# release camera
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Getting corners around the face
+    faces = faceCascade.detectMultiScale(imgGray, 1.3, 5)  # 1.3 = scale factor, 5 = minimum neighbor
+    # drawing bounding box around face
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    '''
+    # detecting eyes
+    eyes = eyeCascade.detectMultiScale(imgGray)
+    # drawing bounding box for eyes
+    for (ex, ey, ew, eh) in eyes:
+        img = cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 3)
+    '''
+
+    cv2.imshow('face_detect', img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
 cap.release()
-
-
-
-   
-   
-# Opening image
-img = cv2.imread("flipped_image.jpg")
-   
-# OpenCV opens images as BRG 
-# but we want it as RGB and 
-# we also need a grayscale 
-# version
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-   
-# Creates the environment 
-# of the picture and shows it
-plt.subplot(1, 1, 1)
-plt.imshow(img_rgb)
-plt.show()
-
-
-
-
-
-
-
-
-
-# Use minSize because for not 
-# bothering with extra-small 
-# dots that would look like STOP signs
-stop_data = cv2.CascadeClassifier('stop_data.xml')
-found = stop_data.detectMultiScale(img_gray,minSize =(20, 20))
-# Don't do anything if there's 
-# no sign
-amount_found = len(found)
-   
-   
-if amount_found != 0:
-       
-    # There may be more than one
-    # sign in the image
-    for (x, y, width, height) in found:
-           
-        # We draw a green rectangle around
-        # every recognized sign
-        cv2.rectangle(img_rgb, (x, y), 
-                      (x + height, y + width), 
-                      (0, 255, 0), 5)
-
-
-# Creates the environment of 
-# the picture and shows it
-plt.subplot(1, 1, 1)
-plt.imshow(img_rgb)
-plt.show()
-
-
+cv2.destroyWindow('face_detect')
