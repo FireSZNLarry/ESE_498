3258d8a2f8e66ad8916a77c6df190b63904b8236
ckrobinsonwustl <ckrobinson@wustl.edu>
2024-03-05 13:45:43 -0600

    Update shape2.py

--- pi_hat_tests/shape2.py
+++ pi_hat_tests/shape2.py
@@ -1,8 +1,25 @@
 # Code from https://www.geeksforgeeks.org/how-to-detect-shapes-in-images-in-python-using-opencv/
 
 import cv2 
+
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+
+# set dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+
+# take frame
+ret, frame = cap.read()
+#flip frame takes pictures right side up
+flipped_frame = cv2.flip(frame,0)
+
+# write frame to file
+cv2.imwrite('image1.jpg', flipped_frame)
+# release camera
+cap.release()
+
 #reading the image  
-image = cv2.imread("example.jpg") 
+image = cv2.imread("image1.jpg") 
 edged = cv2.Canny(image, 10, 250) 
 cv2.imshow("Edges", edged) 
 cv2.waitKey(0) 
