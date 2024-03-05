59217d5d30f00621328f67c7d5a243e856ad31e1
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-03-05 14:18:23 -0600

    Update shape3.py

--- pi_hat_tests/shape3.py
+++ pi_hat_tests/shape3.py
@@ -1,9 +1,23 @@
 import cv2
 
-# Enable camera
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 420)
+# open camera
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+
+# set dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+
+# take frame
+ret, frame = cap.read()
+#flip frame takes pictures right side up
+flipped_frame = cv2.flip(frame,0)
+
+# write frame to file
+cv2.imwrite('flipped_image.jpg', flipped_frame)
+# release camera
+cap.release()
+
+
 
 # import cascade file for facial recognition
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
