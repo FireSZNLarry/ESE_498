6b224287d8916e8c2952b6cab94dfd55af1b43ae
FireSZNLarry <112635784+FireSZNLarry@users.noreply.github.com>
2024-03-15 14:47:48 -0500

    Update camera_test2.py

--- pi_hat_tests/camera_test2.py
+++ pi_hat_tests/camera_test2.py
@@ -30,24 +30,3 @@ cv2.imwrite('flipped_image.jpg', flipped_frame)
 cap.release()
 
 print("Flipped image saved as flipped_image.jpg")
-
-
-/* import cv2
-
-# open camera
-cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
-
-# set dimensions
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
-
-# take frame
-ret, frame = cap.read()
-#flip frame takes pictures right side up
-flipped_frame = cv2.flip(frame,0)
-
-# write frame to file
-cv2.imwrite('flipped_image.jpg', flipped_frame)
-# release camera
-cap.release()
-*/
