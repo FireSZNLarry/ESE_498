07f879ade8651ff132515f57ce454d82d20aaa3d
ckrobinsonwustl <ckrobinson@wustl.edu>
2024-04-18 13:04:02 -0500

    Update color-test.py

--- pi_hat_tests/color-test.py
+++ pi_hat_tests/color-test.py
@@ -27,8 +27,8 @@ IMAGE_WIDTH = 320
 IMAGE_HEIGHT = 240
 fps = 0
 
-hsv_min = np.array((83, 80, 80))
-hsv_max = np.array((120, 255, 255))
+hsv_min = np.array((90, 80, 80))
+hsv_max = np.array((100, 255, 255))
 
 colors = []
 
