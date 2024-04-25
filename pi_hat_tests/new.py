9725ab4d77e81d457aad43d634d8e889e3d97c9e
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-24 20:06:27 -0500

    Update new.py

--- pi_hat_tests/new.py
+++ pi_hat_tests/new.py
@@ -59,21 +59,20 @@ turn_sensitivity = 10  # Higher sensitivity in turning
 
 
 scan_data = [0]*360
-while True:
-    for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan:
-            angle = int(angle)
-            if 80 <= angle < 200:
-                scan_data[angle] = distance
-                if distance < 500:
-                    update_steering_angle(70)
-                    time.sleep(0.1)
-                elif distance < 1000:
-                    update_steering_angle(130)
-                    time.sleep(0.1)
-                else:
-                    update_steering_angle(90)
-                    time.sleep(0.1)
+for scan in lidar.iter_scans():
+    for (_, angle, distance) in scan:
+        angle = int(angle)
+        if 80 <= angle < 200:
+            scan_data[angle] = distance
+            if distance < 500:
+                update_steering_angle(70)
+                time.sleep(0.1)
+            elif distance < 1000:
+                update_steering_angle(130)
+                time.sleep(0.1)
+            else:
+                update_steering_angle(90)
+                time.sleep(0.1)
                
 
 
