f376d917f8ea32b0bda76ec98301390d0e296788
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-24 21:08:43 -0500

    Update newnewnew.py

--- pi_hat_tests/newnewnew.py
+++ pi_hat_tests/newnewnew.py
@@ -57,23 +57,6 @@ def update_steering_angle(angle):
     servo_steering.angle = angle
 
 
-#pylint: disable=redefined-outer-name,global-statement
-def process_data(data):
-    global max_distance
-    lcd.fill((0,0,0))
-    for angle in range(360):
-        distance = data[angle]
-        if distance > 0:                  # ignore initially ungathered data points
-            if distance < 500:
-                max_distance = max([min([5000, distance]), max_distance])
-                radians = angle * pi / 180.0
-                x = distance * cos(radians)
-                y = distance * sin(radians)
-                point = (160 + int(x / max_distance * 119), 120 + int(y / max_distance * 119))
-                lcd.set_at(point, pygame.Color(255, 255, 255))
-    pygame.display.update()
-
-
 scan_data = [0]*360
 
 try:
