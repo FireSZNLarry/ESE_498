512c4f36c8d8257004572ed3146bd741654d721c
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-24 21:00:03 -0500

    Update newnewnew.py

--- pi_hat_tests/newnewnew.py
+++ pi_hat_tests/newnewnew.py
@@ -85,7 +85,10 @@ try:
                 update_steering_angle(70)
                 time.sleep(0.1)
         process_data(scan_data)
-
+        except ValueError:
+        pass
+    except ValueError:
+    pass
 except ValueError:
     pass
 lidar.stop()
