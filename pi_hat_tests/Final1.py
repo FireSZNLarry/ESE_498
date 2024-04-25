1a9de4dd7a129eacaaf7fce5add3e277b8f18c10
ckrobinsonwustl <ckrobinson@wustl.edu>
2024-04-24 19:27:55 -0500

    Update Final1.py

--- pi_hat_tests/Final1.py
+++ pi_hat_tests/Final1.py
@@ -45,7 +45,7 @@ fps = 0
 
 color = input('What object do you want to find? Options are: ibuprofen, nyquil.\n')     # \n ---> newline  ---> It causes a line break 
 print(color)
-if color = 'nyquil':
+if color == 'nyquil':
     hsv_min = np.array((70, 80, 80))
     hsv_max = np.array((100, 255, 255))
     print('nyquil color')
