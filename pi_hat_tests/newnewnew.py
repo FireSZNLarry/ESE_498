a57deb07bbb68f70ca58400606e7f686d17b48dc
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-24 21:03:23 -0500

    Update newnewnew.py

--- pi_hat_tests/newnewnew.py
+++ pi_hat_tests/newnewnew.py
@@ -64,12 +64,16 @@ def process_data(data):
     for angle in range(360):
         distance = data[angle]
         if distance > 0:                  # ignore initially ungathered data points
-            max_distance = max([min([5000, distance]), max_distance])
-            radians = angle * pi / 180.0
-            x = distance * cos(radians)
-            y = distance * sin(radians)
-            point = (160 + int(x / max_distance * 119), 120 + int(y / max_distance * 119))
-            lcd.set_at(point, pygame.Color(255, 255, 255))
+            if distance < 500:
+                print(distance)
+                update_steering_angle(70)
+                time.sleep(0.1)
+                max_distance = max([min([5000, distance]), max_distance])
+                radians = angle * pi / 180.0
+                x = distance * cos(radians)
+                y = distance * sin(radians)
+                point = (160 + int(x / max_distance * 119), 120 + int(y / max_distance * 119))
+                lcd.set_at(point, pygame.Color(255, 255, 255))
     pygame.display.update()
 
 
@@ -80,10 +84,6 @@ try:
     for scan in lidar.iter_scans():
         for (_, angle, distance) in scan:
             scan_data[min([359, floor(angle)])] = distance
-            if distance < 500:
-                print(distance)
-                update_steering_angle(70)
-                time.sleep(0.1)
         process_data(scan_data)
 except ValueError:
     pass
