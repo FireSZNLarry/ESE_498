312a6dd2abed012eceb74944c24855caa3e502ae
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-25 19:53:38 -0500

    Update test_cam.py

--- pi_hat_tests/test_cam.py
+++ pi_hat_tests/test_cam.py
@@ -13,30 +13,18 @@ import motorControl as momo
 i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 100
-#channel_num = 14
 momo.Motor_Speed(pca,0.175)
-
 os.putenv('SDL_FBDEV', '/dev/fb1')
 pygame.init()
-
 PORT_NAME = '/dev/ttyUSB0'
 lidar = RPLidar(None, PORT_NAME, timeout=3)
-
-#i2c_bus = busio.I2C(SCL, SDA)
-#pca = PCA9685(i2c_bus)
-#pca.frequency = 100
 steering_channel = 14
 motor_channel = 15
-
 servo_steering = servo.Servo(pca.channels[steering_channel])
-#motor = pca.channels[motor_channel]
-
 def update_steering_angle(angle):
     servo_steering.angle = angle
-
 def scale_lidar_distance(distance, max_distance=3000):
     return min(distance, max_distance) / max_distance
-
 def main():
     try:
         scan_data = [0]*360
@@ -52,7 +40,6 @@ def main():
                           time.sleep(0.1)
                           if distance < 700:
                               momo.Motor_Speed(pca,0)
-                              
                 for angle in range(360):
                     distance = scan_data[angle]
                     if distance:
@@ -61,13 +48,11 @@ def main():
                         x = scaled_distance * cos(radians) * 119
                         y = scaled_distance * sin(radians) * 119
                         point = (160 + int(x), 120 + int(y))
-
     except KeyboardInterrupt:
         print('Stopping.')
     finally:
         lidar.stop()
         lidar.disconnect()
         pygame.quit()
-
 if __name__ == "__main__":
     main()
