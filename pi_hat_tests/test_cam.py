ac0c8e1d79bf64bf0e03368b98e2c7fd865da6cf
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-25 19:14:43 -0500

    Update test_cam.py

--- pi_hat_tests/test_cam.py
+++ pi_hat_tests/test_cam.py
@@ -48,17 +48,7 @@ def Servo_Motor_Initialization():
     i2c_bus = busio.I2C(SCL,SDA)
     pca = PCA9685(i2c_bus)
     pca.frequency = 100
-    return pca
-
-def Motor_Start(pca):
-    x = input("Press and hold EZ button. Once the LED turns red, immediately relase the button. After the LED blink red once, press 'ENTER'on keyboard.")
-    #Motor_Speed(pca, 0.1)
-    time.sleep(2)
-    y = input("If the LED just blinked TWICE, then press the 'ENTER'on keyboard.")
-    #Motor_Speed(pca, -0.1)
-    time.sleep(2)
-    z = input("Now the LED should be in solid green, indicating the initialization is complete. Press 'ENTER' on keyboard to proceed")
-   
+    return pca 
 
 def Motor_Speed(pca,percent):
     #converts a -1 to 1 value to 16-bit duty cycle
@@ -68,7 +58,6 @@ def Motor_Speed(pca,percent):
        
     #initialization
 pca = Servo_Motor_Initialization()
-Motor_Start(pca)
 
 
 
