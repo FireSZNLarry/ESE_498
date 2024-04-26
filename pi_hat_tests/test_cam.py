238d223bd73bf1562024843e543a9106902d9d90
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-25 19:16:58 -0500

    Update test_cam.py

--- pi_hat_tests/test_cam.py
+++ pi_hat_tests/test_cam.py
@@ -44,21 +44,12 @@ def update_steering_angle(angle):
 def scale_lidar_distance(distance, max_distance=4000):
     return min(distance, max_distance) / max_distance
 
-def Servo_Motor_Initialization():
-    i2c_bus = busio.I2C(SCL,SDA)
-    pca = PCA9685(i2c_bus)
-    pca.frequency = 100
-    return pca 
 
 def Motor_Speed(pca,percent):
     #converts a -1 to 1 value to 16-bit duty cycle
     speed = ((percent) * 3277) + 65535 * 0.15
     pca.channels[15].duty_cycle = math.floor(speed)
     #print(speed/65535)
-       
-    #initialization
-pca = Servo_Motor_Initialization()
-
 
 
 ##
