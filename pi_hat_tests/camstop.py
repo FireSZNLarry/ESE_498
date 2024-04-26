7ed7c444a7c0a27b675343cac2f9004e4636988b
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-25 22:38:01 -0500

    Update camstop.py

--- pi_hat_tests/camstop.py
+++ pi_hat_tests/camstop.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
                             for scan in lidar.iter_scans():
                                 for (_, angle, distance) in scan:
                                     angle = int(angle)
-                                    if 130 <= angle < 150:
+                                    if 90 <= angle < 110:
                                         scan_data[angle] = distance
                                         print(distance)
                                         if distance < 3000:
