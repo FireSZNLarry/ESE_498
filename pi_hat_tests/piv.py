705bb0d9ba46eb91f2242b8d8d7d4de37cd07e3e
CameronKRobinson <112365223+CameronKRobinson@users.noreply.github.com>
2024-04-26 09:48:39 -0500

    Create piv.py

---
+++ pi_hat_tests/piv.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import time
+from time import sleep
+import math
+from board import SCL,SDA
+import busio
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+import os
+from math import cos, sin, pi, floor
+import pygame
+from adafruit_rplidar import RPLidar
+import threading
+import queue
+import motorControl as momo
+
+i2c = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c)
+pca.frequency = 100
+channel_num = 14
+servo7 = servo.Servo(pca.channels[channel_num])
+os.putenv('SDL_FBDEV', '/dev/fb1')
+pygame.init()
+PORT_NAME = '/dev/ttyUSB0'
+lidar = RPLidar(None, PORT_NAME, timeout=3)
+steering_channel = 14
+motor_channel = 15
+servo_steering = servo.Servo(pca.channels[steering_channel])
+
+
+momo.Motor_Speed(pca,0)
+time.sleep(0.5)
+momo.Motor_Speed(pca,0.15)
+time.sleep(0.5)
+momo.Motor_Speed(pca,0.15)
+time.sleep(0.5)
+momo.Motor_Speed(pca,0.15)
+
+
+
+
+
